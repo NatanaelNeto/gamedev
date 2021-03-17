d6d334192b93cc2e8ab572cb16af5f80489efcca
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-03-17 10:45:00 -0300

    Continuação página home

---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <h3>Título</h3>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ src/data/index.js
@@ -0,0 +1,34 @@
+const jobs = [
+  {
+    id: 0,
+    job: 'Revisor de Código',
+    company: 'Trybe',
+    description: 'Empresa de educação na área de TI',
+    period: '01/2021 - Presente',
+    tasks: [
+      'Revisão e criação de projetos',
+      'Revisão e criação de exercícios',
+      'Revisão e criação de conteúdo do curso',
+    ],
+  },
+  {
+    id: 1,
+    job: 'Desenvolvedor Front-end (Trainee)',
+    company: 'Inovaula',
+    description: 'Plataforma de suporte para professores',
+    period: '09/2017 - 12/2017',
+    tasks: [
+      'Desenvolvimento em vue.js',
+      'Testes de usuário',
+    ],
+  },
+];
+
+const projects = [
+  {
+    id: 0,
+
+  },
+];
+
+export { jobs, projects };
