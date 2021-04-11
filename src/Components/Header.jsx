8f38926483315a616622c7e3e5fcad58bb912c16
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-04-11 00:18:43 -0300

    Estilizando header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 class Header extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.changeBackground);
+  }
+
+  changeBackground() {
+    const header = document.getElementById('header');
+    const viewport = window.innerHeight;
+
+    header.style.background = window.pageYOffset > viewport
+      ? 'linear-gradient(0deg, rgba(20,20,20,0.9) 0%, rgba(20,20,20,1) 100%)'
+      : 'linear-gradient(0deg, rgba(20,20,20,0) 0%, rgba(20,20,20,1) 100%)'
+  }
+
   render() {
     return (
-      <header>
+      <header id="header">
         <nav>
           <ul>
           <Link to="/"><li id="menuHome">Home</li></Link>
--- src/Pages/Contato.jsx
+++ src/Pages/Contato.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import resume from '../data/resume.pdf';
 
 class Contato extends React.Component {
   render () {
     return (
-      <div>Contato</div>
+      <section className="section-opening contato-section">
+        <h1>CONTATO</h1>
+        <div className="contato-content">
+          <h3>Currículo</h3>
+          <a href={ resume } target="_blank" rel="noopener noreferrer">Acesse aqui</a>
+        </div>
+      </section>
     );
   }
 }
