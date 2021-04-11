45cb22b43e73e3ed49e7ef86e1158e15603d13a1
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-04-11 01:35:55 -0300

    mobile version feito

--- src/Pages/Contato.jsx
+++ src/Pages/Contato.jsx
@@ -3,12 +3,29 @@ import resume from '../data/resume.pdf';
 
 class Contato extends React.Component {
   render () {
+    const insta = 'https://www.instagram.com/nesn.gamedev/';
+    const github = 'https://github.com/natanaelneto';
+    const whats = ' https://wa.me/5581998433904/?text=Ol%C3%A1%21%20Vi%20seu%20curr%C3%ADculo%20e%20quero%20entrar%20em%20contato%20com%20voc%C3%AA%21';
     return (
       <section className="section-opening contato-section">
         <h1>CONTATO</h1>
         <div className="contato-content">
-          <h3>Currículo</h3>
-          <a href={ resume } target="_blank" rel="noopener noreferrer">Acesse aqui</a>
+          <div className="contato-row">
+            <h3>Currículo</h3>
+            <a href={ resume } target="_blank" rel="noopener noreferrer">download aqui</a>
+          </div>
+          <div className="contato-row">
+            <h3>Instagram</h3>
+            <a href={ insta } target="_blank" rel="noopener noreferrer">@nesn.gamedev</a>
+          </div>
+          <div className="contato-row">
+            <h3>Github</h3>
+            <a href={ github } target="_blank" rel="noopener noreferrer">NatanaelNeto</a>
+          </div>
+          <div className="contato-row">
+            <h3>WhatsApp</h3>
+            <a href={ whats } target="_blank" rel="noopener noreferrer">(81) 998-433-904</a>
+          </div>
         </div>
       </section>
     );
